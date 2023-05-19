dbd08dbe3e9d3c2d6c98f2dd0b9709880a507d4b
0H00N <yhoonyh@gmail.com>
2023-05-19 20:52:03 +0900

    [level 2] Title: 올바른 괄호, Time: 8.94 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = True
+    stack = []
+    for p in s:
+        if p == '(':
+            stack.append(p)
+        else:
+            if len(stack) > 0:
+                stack.pop()
+            else:
+                return False
+    
+    return len(stack) == 0
