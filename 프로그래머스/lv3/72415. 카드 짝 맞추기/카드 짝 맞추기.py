f66d5bbe4d642ef0dec0d6ad3858c0fbaf9ce424
0H00N <yhoonyh@gmail.com>
2023-06-07 23:22:37 +0900

    [level 3] Title: 카드 짝 맞추기, Time: 3.86 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv3/72415. 카드 짝 맞추기/카드 짝 맞추기.py
+++ 프로그래머스/lv3/72415. 카드 짝 맞추기/카드 짝 맞추기.py
@@ -1,63 +1,45 @@
 from copy import deepcopy
-from itertools import permutations
-
-def check(board, r, c, d):
-    n_0 = 0
-    if r > 0:
-        if d[r-1][c] == -1:
-            d[r-1][c] = d[r][c] + 1
-            n_0 += 1
-        for row in range(r-1, -1, -1):
-            if board[row][c] > 0 or row==0:
-                if d[row][c] == -1:
-                    n_0 += 1
-                    d[row][c] = d[r][c] + 1
-                break
-    if r < 3:
-        if d[r+1][c] == -1:
-            d[r+1][c] = d[r][c] + 1
-            n_0 += 1
-        for row in range(r+1, 4):
-            if board[row][c] > 0 or row==3:
-                if d[row][c] == -1:
-                    n_0 += 1
-                    d[row][c] = d[r][c] + 1
-                break
-    if c > 0:
-        if d[r][c-1] == -1:
-            d[r][c-1] = d[r][c] + 1
-            n_0 += 1
-        for col in range(c-1, -1, -1):
-            if board[r][col] > 0 or col==0:
-                if d[r][col] == -1:
-                    n_0 += 1
-                    d[r][col] = d[r][c] + 1
-                break
-    if c < 3:
-        if d[r][c+1] == -1:
-            d[r][c+1] = d[r][c] + 1
-            n_0 += 1
-        for col in range(c+1, 4):
-            if board[r][col] > 0 or col==3:
-                if d[r][col] == -1:
-                    n_0 += 1
-                    d[r][col] = d[r][c] + 1
-                break
-    return n_0
-
 def get_d(board, r, c):
     d = [[-1]*4 for _ in range(4)]
-    d[r][c] = 0
+    d[r][c] = 100
     n_0 = 15
-    ii = 0
+    for row in range(r-1, -1, -1):
+        d[row][c] = 1
+        n_0 -= 1
+        if board[row][c] > 0:
+            break
+    for row in range(r+1, 4):
+        d[row][c] = 1
+        n_0 -= 1
+        if board[row][c] > 0:
+            break
+    for col in range(c-1, -1, -1):
+        d[r][col] = 1
+        n_0 -= 1
+        if board[r][col] > 0:
+            break
+    for col in range(c+1, 4):
+        d[r][col] = 1
+        n_0 -= 1
+        if board[r][col] > 0:
+            break
     while n_0 > 0:
         old_d = deepcopy(d)
         for i in range(4):
             for j in range(4):
-                if old_d[i][j] == ii:
-                    n_0 -= check(board, i, j, d)
-        ii += 1
-                    
+                if old_d[i][j] > 0:
+                    if i > 0 and d[i-1][j] == -1:
+                        d[i-1][j] = old_d[i][j]+1
+                        n_0 -= 1
+                    if i < 3 and d[i+1][j] == -1:
+                        d[i+1][j] = old_d[i][j]+1
+                        n_0 -= 1
+                    if j > 0 and d[i][j-1] == -1:
+                        d[i][j-1] = old_d[i][j]+1
+                        n_0 -= 1
+                    if j < 3 and d[i][j+1] == -1:
+                        d[i][j+1] = old_d[i][j]+1
+                        n_0 -= 1
     return d
                         
 def find_next(board, d):
@@ -73,53 +55,49 @@ def find_next(board, d):
     return min_n, min_rc
 
 def find_n(board, n):
-    rc = []
     for i in range(4):
         for j in range(4):
             if board[i][j] == n:
-                rc.append((i, j))
-    return rc
+                return i, j
             
 def solution(board, r, c):
     total_n = 0
-    card_list = []
     for i in range(4):
         for j in range(4):
-            if board[i][j] > 0 and board[i][j] not in card_list:
-                card_list.append(board[i][j])
+            if board[i][j] > 0:
+                total_n += 1
     
-    min_d = 10000
-    r_ori = r
-    c_ori = c
-    for card_order in permutations(card_list, len(card_list)):
-        b = deepcopy(board)
-        total = 0
-        r = r_ori
-        c = c_ori
-        for n in card_order:
-            d = get_d(b, r, c)
-            rc = find_n(b, n)
+    answer = total_n
+    to_find = 0
+    
+    while total_n > 0:
+        
+        d = get_d(board, r, c)
+        if board[r][c] > 0:
+            to_find = board[r][c]
+            board[r][c] = 0
+            total_n -= 1
+            for b in board:
+                print(b)
+            print(r, c, answer)
+        else:
+            to_find, rc = find_next(board, d)
+            answer += d[rc[0]][rc[1]]
+            total_n -= 1
             
-            tmp_b = deepcopy(b)
-            tmp_b[rc[0][0]][rc[0][1]] = 0
-            d1 = d[rc[0][0]][rc[0][1]] + get_d(tmp_b, rc[0][0], rc[0][1])[rc[1][0]][rc[1][1]]
-            tmp_b = deepcopy(b)
-            tmp_b[rc[1][0]][rc[1][1]] = 0
-            d2 = d[rc[1][0]][rc[1][1]] + get_d(tmp_b, rc[1][0], rc[1][1])[rc[0][0]][rc[0][1]]
-            if d1 < d2:
-                total += d1
-                b[rc[0][0]][rc[0][1]] = 0
-                b[rc[1][0]][rc[1][1]] = 0
-                r = rc[1][0]
-                c = rc[1][1]
-            else:
-                total += d2
-                b[rc[0][0]][rc[0][1]] = 0
-                b[rc[1][0]][rc[1][1]] = 0
-                r = rc[0][0]
-                c = rc[0][1]
-        if total < min_d:
-            min_d = total
-    answer = min_d + len(card_list)*2
-    
+            r = rc[0]
+            c = rc[1]
+            board[r][c] = 0
+            d = get_d(board, r, c)
+            for b in board:
+                print(b)
+            print(r, c, answer)
+        r, c = find_n(board, to_find)
+        answer += d[r][c]
+        board[r][c] = 0
+        total_n -= 1
+        to_find = 0
+        for b in board:
+            print(b)
+        print(r, c, answer)
     return answer
